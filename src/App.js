deb004f74bef2b6772b96f8b8aaebcb02e64a169
Lamia Rimty <rimtycse@gmail.com>
2021-11-25 09:22:41 +0600

    Observing_the_last

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ export default function App() {
 
   const [query,setQuery]=useState('')
   const [pageNumber,setPageNumber]=useState(1)
+  const { loading,error,books,hasMore} = useBookSearch(query,pageNumber)
+  
 
   const observer = useRef()
   const lastBookElementRef= useCallback(
     (node) => { 
-      console.log(node)
-    })
+
+     if(loading) return
+     if(observer.current)observer.current.disconnect()
+
+     observer.current=new IntersectionObserver( entries=>{
+       if(entries[0].isIntersecting){ //node that observing
+         console.log('Visible') //evryting is  watching to be in the entries array
+       }
+     })
+
+     if(node)observer.current.observe(node) //we want to observe our node
+    },[loading,hasMore])  
 
   function handleSearch(e){
     setQuery(e.target.value)
     setPageNumber(1)
   }
 
-  const { loading,error,books,hasMore} = useBookSearch(query,pageNumber)
-  return (
+return (
     <>
       <input type="text" value={query} onChange={handleSearch}></input>
      {books.map((book,index)=>{
